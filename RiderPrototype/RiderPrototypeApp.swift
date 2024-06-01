9388798bee874a659ad67f823811ca98e18c7f32
Gagan Pareek <pareekgagan2018@gmail.com>
2024-06-01 17:48:52 +0530

    Initial Commit

---
+++ RiderPrototype/RiderPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiderPrototypeApp.swift
+//  RiderPrototype
+//
+//  Created by Gagan Pareek on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RiderPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
